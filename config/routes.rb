e9000c8740587d2a322a99c5b96895f2aca50980
bongit <celendipity@gmail.com>
2014-09-12 18:17:29 +0900

    tutorial finish

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,16 @@ Rails.application.routes.draw do
       get 'download'
     end
   end
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
